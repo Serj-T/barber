b438571e6f1fe4861a1f70f8fc233f4ab7d7ffb2
Serj T <serjtelepov@gmail.com>
2016-10-25 20:32:37 +0300

    Форма входа на js

---
+++ js/login.js
@@ -0,0 +1,48 @@
+var link = document.querySelector(".login");
+var popup = document.querySelector(".modal");
+var close = document.querySelector(".modal-close")
+var login = popup.querySelector("[name=user-login]");
+var password = popup.querySelector("[name=user-pass]");
+var form = popup.querySelector("form");
+var storage = localStorage.getItem("login");
+
+link.addEventListener("click", function(event) {
+	event.preventDefault();
+	popup.classList.add("modal-on");
+	if (storage) {
+		login.value = storage;
+		password.focus();
+		console.log("пароль фокус");
+	} else {
+		login.focus();
+		console.log("логин фокус");
+	}
+});
+
+close.addEventListener("click", function(event) {
+	event.preventDefault();
+	popup.classList.remove("modal-on");
+	popup.classList.remove("modal-error");
+});
+
+
+form.addEventListener("submit", function(event) {
+	if (!login.value || !password.value) {
+		event.preventDefault();
+		popup.classList.remove("modal-error");
+		popup.offsetWidth = popup.offsetWidth;
+		popup.classList.add("modal-error");
+	} else {
+		localStorage.setItem("login", login.value);
+	}
+});
+
+window.addEventListener("keydown", function(event) {
+	if (popup.classList.contains("modal-on")) {
+		console.log("Нет класса");
+		if (event.keyCode === 27) {
+			popup.classList.remove("modal-on");
+			popup.classList.remove("modal-error");
+		}
+	}
+});
