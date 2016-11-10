c14912d2b33da8642c7e44c89bc57de5939145bd
Serj T <serjtelepov@gmail.com>
2016-11-10 17:38:22 +0300

    +prefix +cssmini

--- js/map.js
+++ js/map.js
@@ -1,23 +1,31 @@
 var mapLink = document.querySelector(".map");
 var mapModal = document.querySelector(".modal-map");
 var mapClose = document.querySelector(".close-map");
-
+var overlay = document.querySelector(".overlay");
 
 mapLink.addEventListener("click", function(event) {
 	event.preventDefault();
 	mapModal.classList.add("modal-map-on");
+	overlay.classList.add("overlay-on");
 });
 
 mapClose.addEventListener("click", function(event) {
 	event.preventDefault();
 	mapModal.classList.remove("modal-map-on");
+	overlay.classList.remove("overlay-on");
+});
+
+overlay.addEventListener("click", function(event) {
+	event.preventDefault();
+	mapModal.classList.remove("modal-map-on");
+	overlay.classList.remove("overlay-on");
 });
 
 window.addEventListener("keydown", function(event) {
 	if (mapModal.classList.contains("modal-map-on")) {
-		console.log("WHAT?");
 		if (event.keyCode === 27) {
 			mapModal.classList.remove("modal-map-on");
+			overlay.classList.remove("overlay-on");
 		}
 	}
 });
